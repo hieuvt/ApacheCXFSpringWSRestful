dbce7250eac2a99231100da3b0306bbf7f00512e
unknown <hieuvt2008@gmail.com>
2014-12-07 15:32:36 +0700

    Add AngularJS Client

--- src/main/java/com/imipgroup/hieuvt/BookService.java
+++ src/main/java/com/imipgroup/hieuvt/BookService.java
@@ -1,5 +1,7 @@
 package com.imipgroup.hieuvt;
 
+import org.apache.cxf.rs.security.cors.CorsHeaderConstants;
+import org.apache.cxf.rs.security.cors.CrossOriginResourceSharing;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,19 +13,20 @@ import java.net.URLDecoder;
 /**
  * Created by hieu.vutrong on 10/28/2014.
  */
+//@CrossOriginResourceSharing(allowAllOrigins = true)
 public class BookService {
 
     protected final Logger log = LoggerFactory.getLogger(BookService.class);
 
 
-    @POST
+//    @POST
+    @GET
     @Path("/getbook/{name}")
-    @Produces({"application/xml","application/json"})
+    @Produces({"application/json"})
     @Consumes({"application/xml","application/json","application/x-www-form-urlencoded"})
     public Response getBucket(@PathParam("name") String name) {
         log.debug("name : " + name);
         BookVO bookVO = null;
-
         try {
             bookVO = BookDB.getInstance().getBook(name);
         } catch (Exception e) {
@@ -33,7 +36,9 @@ public class BookService {
         if(bookVO == null){
             return Response.status(Response.Status.BAD_REQUEST).build();
         }else{
-            return Response.ok(bookVO).build();
+            return Response.ok(bookVO)
+                    .header(CorsHeaderConstants.HEADER_AC_ALLOW_ORIGIN, "*")
+                    .build();
         }
     }
 }
--- src/test/java/com/imipgroup/hieuvt/BookServiceTest.java
+++ src/test/java/com/imipgroup/hieuvt/BookServiceTest.java
@@ -3,6 +3,7 @@ package com.imipgroup.hieuvt;
 import junit.framework.TestCase;
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 
 import java.net.URLEncoder;
@@ -20,20 +21,33 @@ public class BookServiceTest extends TestCase {
             String url = "http://localhost:8080/bookservice/getbook/";
 
             url = url + URLEncoder.encode(bookName, "UTF-8");
+            System.out.println(url);
 
             HttpClient client = new HttpClient();
-            PostMethod mPost = new PostMethod(url);
-            client.executeMethod(mPost);
+
+//            PostMethod mPost = new PostMethod(url);
+//            client.executeMethod(mPost);
+
+            GetMethod mGet = new GetMethod(url);
+            client.executeMethod(mGet);
+
             Header mtHeader = new Header();
             mtHeader.setName("content-type");
             mtHeader.setValue("application/x-www-form-urlencoded");
             mtHeader.setName("accept");
 //            mtHeader.setValue("application/xml");
             mtHeader.setValue("application/json");
-            mPost.addRequestHeader(mtHeader);
-            client.executeMethod(mPost);
-            output = mPost.getResponseBodyAsString();
-            mPost.releaseConnection();
+
+//            mPost.addRequestHeader(mtHeader);
+//            client.executeMethod(mPost);
+//            output = mPost.getResponseBodyAsString();
+//            mPost.releaseConnection();
+
+            mGet.addRequestHeader(mtHeader);
+            client.executeMethod(mGet);
+            output = mGet.getResponseBodyAsString();
+            mGet.releaseConnection();
+
            System.out.println(output);
 //            assertEquals(output,expectedOutput);
         } catch (Exception e) {
